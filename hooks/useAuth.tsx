c33f02b2dbe64dd3aefa54bd84474da26d5e06dc
1barada <jionanrouk@gmail.com>
2024-02-27 13:40:07 +0200

    removed unnecessary useCallbacks, added errors handling

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,75 +1,92 @@
-'use client'
+"use client";
 
-import { UserInfo } from "@/types";
+import { createContext,  useContext,  useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+
+import { UserInfo } from "@/types/user";
 
 interface AuthContextType {
   user?: UserInfo;
   isLoading: boolean;
-  signIn: () => void;
-  signOut: () => void;
+  error?: Error ;
+  signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export function AuthProvider({children}: {
-  children: React.ReactNode
-}) {
+export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<UserInfo | undefined>(undefined);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | undefined>(undefined);
 
   useEffect(() => {
     const supabase = createClient();
-    const {data: authListener} = supabase.auth.onAuthStateChange((event, session) => {
-      if (session) {
-        const userInfo: UserInfo = {
-          id: session.user.id,
-          name: session.user.identities?.[0].identity_data?.name,
-          avatarUrl: session.user.identities?.[0].identity_data?.avatar_url,
-        };
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (session) {
+          const userInfo: UserInfo = {
+            id: session.user.id,
+            name: session.user.identities?.[0].identity_data?.name,
+            avatarUrl: session.user.identities?.[0].identity_data?.avatar_url,
+          };
 
-        setUser(userInfo);
-      } else {
-        setUser(undefined);
+          setUser(userInfo);
+        } else {
+          setUser(undefined);
+        }
+
+        if (event === "INITIAL_SESSION") setIsLoading(false);
       }
-      setIsLoading(false);
-    });
+    );
 
     return () => authListener.subscription.unsubscribe();
   }, []);
 
-  const signIn = useCallback(async () => {
+  async function signIn() {
+    if (isLoading) return;
     setIsLoading(true);
-    const supabase = createClient();
-    await supabase.auth.signInWithOAuth({provider: 'google'});
-    setIsLoading(false)
-  }, [user]);
-  
-  const signOut = useCallback(async () => {
-    setIsLoading(true);
-    const supabase = createClient();
-    await supabase.auth.signOut();
-    setIsLoading(false)
-  }, [user]);
 
-  return <AuthContext.Provider 
-    value={{
-      user,
-      isLoading,
-      signIn,
-      signOut
-    }}
-    children={children}
-  />
+    const supabase = createClient();
+    const {error} = await supabase.auth.signInWithOAuth({ provider: "google" });
+
+    if (error) setError(error);
+    
+    setIsLoading(false);
+  };
+
+  async function signOut() {
+    if (isLoading) return;
+    setIsLoading(true);
+
+    const supabase = createClient();
+    const {error} = await supabase.auth.signOut();
+
+    if (error) setError(error);
+
+    setIsLoading(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLoading,
+        error,
+        signIn,
+        signOut
+      }}
+      children={children}
+    />
+  );
 }
 
 export function useAuth() {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must me used within a AuthProvider');
+    throw new Error("useAuth must me used within a AuthProvider");
   }
-  
+
   return context;
 }
